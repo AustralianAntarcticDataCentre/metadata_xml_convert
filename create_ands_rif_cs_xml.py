4e1985a0f69da6b8e2261795d2d710d86382596a
Matt McKellar-Spence <Matt.McKellar-Spence@antarctica.gov.au>
2014-12-04 10:39:37 +1100

    Add ANDS RIF-CS conversion script

---
+++ create_ands_rif_cs_xml.py
@@ -0,0 +1,39 @@
+"""
+Create an ANDS RIF-CS XML file.
+
+Links
+-----
+
+- http://ands.org.au/guides/cpguide/cpgrifcs.html
+
+- http://services.ands.org.au/documentation/rifcs/guidelines/rif-cs.html
+
+- http://www.ands.org.au/resource/rif-cs.html
+"""
+
+import logging
+import os
+
+from settings import (
+	ANDS_XML_FILE_NAME, ANDS_XML_FOLDER_PATH, ANDS_XML_START, ANDS_XML_STOP
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+	with open(ANDS_XML_FILE_NAME, 'w') as w:
+		w.write(ANDS_XML_START)
+
+		for file_path in os.listdir(ANDS_XML_FOLDER_PATH):
+			with open(file_path) as r:
+				w.write(r.read())
+
+		w.write(ANDS_XML_STOP)
+
+
+if '__main__' == __name__:
+	logging.basicConfig(level=logging.DEBUG)
+
+	main()
