007ccc5f01b945b78142871970449a9bb2f3540d
Matt McKellar-Spence <Matt.McKellar-Spence@antarctica.gov.au>
2014-12-03 15:03:51 +1100

    Move more functions to file check module.
    Moved get output name and find updated files functions.

--- file_checks.py
+++ file_checks.py
@@ -1,10 +1,39 @@
 import logging
 import os
 
-from settings import ADD_FOLDER_TO_FILE_NAME, UPLOAD_PATH
+from settings import (
+	ADD_FOLDER_TO_FILE_NAME, CONVERSIONS, EXPORT_PATH, UPLOAD_PATH, XSL_PATH
+)
 
 
 logger = logging.getLogger(__name__)
+
+
+def append_folder_to_file_name(file_path):
+	"""
+	Add the parent folder name between the file name and extension.
+
+	So "/grandparent/parent/name.ext"
+	Becomes "/grandparent/parent/name-parent.ext".
+
+	This allows the file names to be unique if the parent folders are
+	merged into one folder.
+	"""
+
+	# ["/grandparent/parent", "name.ext"]
+	folder_path, file_name = os.path.split(file_path)
+
+	# ["/grandparent", "parent"]
+	grandparent_path, parent_name = os.path.split(folder_path)
+
+	# ["name", ".ext"]
+	file_base_name, file_ext = os.path.splitext(file_name)
+
+	# ["name", "-", "parent", ".ext"]
+	new_name = ''.join([file_base_name, '-', parent_name, file_ext])
+
+	# ["/grandparent/parent", "name-parent.ext"]
+	return os.path.join(folder_path, new_name)
 
 
 def check_paths(*paths):
@@ -28,6 +57,62 @@ def file_is_newer(newer_file, older_file):
 
 	return os.path.getmtime(newer_file) > os.path.getmtime(newer_file)
 	#return os.path.getctime(newer_file) > os.path.getctime(newer_file)
+
+
+def find_updated_files(force_conversion=False):
+	"""
+	Generate names of XML files that need to be converted.
+
+	The "allowed" function still applies if `force_conversion` is True.
+	"""
+
+	xsl_path = lambda xsl: os.path.join(XSL_PATH, xsl)
+	out_path = lambda folder: os.path.join(EXPORT_PATH, folder)
+
+	# Only include conversions where the XSL and output folder exist.
+	conversions = [
+		(xsl_path(x), out_path(o), f)
+		for x, o, f in CONVERSIONS
+		if check_paths(xsl_path(x), out_path(o))
+	]
+
+	# Loop the contents of the input folder.
+	for file_name in os.listdir(UPLOAD_PATH):
+		# Ignore non-xml files.
+		if not file_name.endswith('.xml'):
+			continue
+
+		# Get the path to the current file.
+		input_file = os.path.join(UPLOAD_PATH, file_name)
+
+		# Loop each of the conversion types.
+		for xsl_path, output_folder, allowed in conversions:
+			# Check for a function to check if conversion is allowed.
+			if allowed is not None:
+				# If conversion not allowed, skip to next item.
+				if not allowed(input_file):
+					continue
+
+			# File path that will be output by the conversion.
+			output_file = os.path.join(output_folder, file_name)
+
+			# Folder to store any failed conversions.
+			err_file = os.path.join(output_folder + '-error', file_name)
+
+			# Tuple with file paths used in the conversion.
+			paths = (input_file, xsl_path, output_file, err_file)
+
+			# Do conversion if it is forced.
+			if force_conversion:
+				yield paths
+
+			# Do conversion if it has not been done before.
+			elif not os.path.exists(output_file):
+				yield paths
+
+			# Do conversion if the input file is newer.
+			elif file_is_newer(input_file, output_file):
+				yield paths
 
 
 def get_files_in_folder(folder, ext=''):
--- main.py
+++ main.py
@@ -5,67 +5,10 @@ from subprocess import call
 from uuid import uuid4
 
 from conversion_calls import get_msxsl_call
-from file_checks import check_paths, file_is_newer
-from settings import CONVERSIONS, EXPORT_PATH, UPLOAD_PATH, XSL_PATH
+from file_checks import append_folder_to_file_name, find_updated_files
 
 
 logger = logging.getLogger(__name__)
-
-
-def find_updated_files(force_conversion=False):
-	"""
-	Generate names of XML files that need to be converted.
-
-	The "allowed" function still applies if `force_conversion` is True.
-	"""
-
-	xsl_path = lambda xsl: os.path.join(XSL_PATH, xsl)
-	out_path = lambda folder: os.path.join(EXPORT_PATH, folder)
-
-	# Only include conversions where the XSL and output folder exist.
-	conversions = [
-		(xsl_path(x), out_path(o), f)
-		for x, o, f in CONVERSIONS
-		if check_paths(xsl_path(x), out_path(o))
-	]
-
-	# Loop the contents of the input folder.
-	for file_name in os.listdir(UPLOAD_PATH):
-		# Ignore non-xml files.
-		if not file_name.endswith('.xml'):
-			continue
-
-		# Get the path to the current file.
-		input_file = os.path.join(UPLOAD_PATH, file_name)
-
-		# Loop each of the conversion types.
-		for xsl_path, output_folder, allowed in conversions:
-			# Check for a function to check if conversion is allowed.
-			if allowed is not None:
-				# If conversion not allowed, skip to next item.
-				if not allowed(input_file):
-					continue
-
-			# File path that will be output by the conversion.
-			output_file = os.path.join(output_folder, file_name)
-
-			# Folder to store any failed conversions.
-			err_file = os.path.join(output_folder + '-error', file_name)
-
-			# Tuple with file paths used in the conversion.
-			paths = (input_file, xsl_path, output_file, err_file)
-
-			# Do conversion if it is forced.
-			if force_conversion:
-				yield paths
-
-			# Do conversion if it has not been done before.
-			elif not os.path.exists(output_file):
-				yield paths
-
-			# Do conversion if the input file is newer.
-			elif file_is_newer(input_file, output_file):
-				yield paths
 
 
 def add_random_uuid_to_file(file_name):
@@ -83,33 +26,6 @@ def add_random_uuid_to_file(file_name):
 
 		with open(file_name, 'w') as h:
 			h.write(new_content)
-
-
-def append_folder_to_file_name(file_path):
-	"""
-	Add the parent folder name between the file name and extension.
-
-	So "/grandparent/parent/name.ext"
-	Becomes "/grandparent/parent/name-parent.ext".
-
-	This allows the file names to be unique if the parent folders are
-	merged into one folder.
-	"""
-
-	# ["/grandparent/parent", "name.ext"]
-	folder_path, file_name = os.path.split(file_path)
-
-	# ["/grandparent", "parent"]
-	grandparent_path, parent_name = os.path.split(folder_path)
-
-	# ["name", ".ext"]
-	file_base_name, file_ext = os.path.splitext(file_name)
-
-	# ["name", "-", "parent", ".ext"]
-	new_name = ''.join([file_base_name, '-', parent_name, file_ext])
-
-	# ["/grandparent/parent", "name-parent.ext"]
-	return os.path.join(folder_path, new_name)
 
 
 def get_arg_parser():
@@ -191,7 +107,7 @@ if __name__ == '__main__':
 			# Move file to the error folder if an error code was returned.
 			if result != 0 and error_file is not None:
 				os.rename(input_file, error_file)
-				logger.error('Conversion failed. moving file to %s', error_file)
+				logger.error('Conversion failed. Moving file to %s', error_file)
 
 			else:
 				# Insert random UUIDs into converted files.
