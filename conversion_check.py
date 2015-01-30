d015d083b3a3017afb5183c140b6ea058c544797
Matt McKellar-Spence <Matt.McKellar-Spence@antarctica.gov.au>
2015-01-30 15:16:56 +1100

    Add private tag checks

--- conversion_check.py
+++ conversion_check.py
@@ -1,9 +1,17 @@
+def check_ands_rif_cs(file_path):
+	folder_path, file_name = os.path.split(file_path)
+	base_name, ext_name = os.path.splitext(file_name)
+	return not base_name.endswith('AAD_RIFCS_ISO')
+
+
 def check_iso_mcp(input_file):
 	"""
 	Checks if MCP conversion is allowed for the given file.
 
 	MCP files are only created if the DIF has an ISO Topic Category of
 	"OCEANS".
+
+	Also makes sure the record is not private.
 	"""
 
 	allowed = False
@@ -14,15 +22,31 @@ def check_iso_mcp(input_file):
 	# will not be marked OK for conversion.
 	oceans_tag = '>OCEANS</ISO_Topic_Category>'
 
+	private_tag = '<Private>TRUE</Private>'
+
 	with open(input_file) as r:
 		content = r.read()
+
 		if 0 <= content.find(oceans_tag):
 			allowed = True
+
+		if 0 <= content.find(private_tag):
+			allowed = False
 
 	return allowed
 
 
-def check_ands_rif_cs(file_path):
-	folder_path, file_name = os.path.split(file_path)
-	base_name, ext_name = os.path.splitext(file_name)
-	return not base_name.endswith('AAD_RIFCS_ISO')
+def check_not_private(input_file):
+	"""
+	Check file does not include the Private tag.
+	"""
+
+	allowed = True
+	private_tag = '<Private>TRUE</Private>'
+
+	with open(input_file) as r:
+		content = r.read()
+		if 0 <= content.find(private_tag):
+			allowed = False
+
+	return allowed
